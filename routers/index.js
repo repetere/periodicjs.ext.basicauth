24909028ea810654d844a668f2c27de93ce2dddd
yaw joseph etse <yaw.etse@gmail.com>
2017-10-20 10:36:52 -0400

    updated catchall route

--- routers/index.js
+++ routers/index.js
@@ -6,7 +6,7 @@ const controllers = require('../controllers');
 const basicauthSettings = periodic.settings.extensions['periodicjs.ext.basicauth'];
 
 if (basicauthSettings.use_global_basic_access_auth) {
-  extensionRouter[(basicauthSettings.use_auth_on_all_routes)?'use':'get'](controllers.auth.ensureAuthentication);
+  extensionRouter[ (basicauthSettings.use_auth_on_all_routes) ? 'use' : 'get' ]('*', controllers.auth.ensureAuthentication);
 }
 
 module.exports = extensionRouter;
